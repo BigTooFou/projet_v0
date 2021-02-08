222e72f3f6932c0df8a6ea21c3290f78dd274207
Paul Faurie <paul.faurie@yahoo.fr>
2021-02-08 21:52:04 +0100

    test sans covid

--- server.js
+++ server.js
@@ -14,32 +14,3 @@ app.get('/', (request, response) => {
 
 app.listen(2502)
 
-
-const http = require("https");
-
-const options = {
-	"method": "GET",
-	"hostname": "covid-19-data.p.rapidapi.com",
-	"port": null,
-	"path": "/totals",
-	"headers": {
-		"x-rapidapi-key": "1632b11b8amsh5d892a7d1c73fdep19210djsn35cac2f2fa82",
-		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
-		"useQueryString": true
-	}
-};
-
-const req = http.request(options, function (res) {
-	const chunks = [];
-
-	res.on("data", function (chunk) {
-		chunks.push(chunk);
-	});
-
-	res.on("end", function () {
-		const body = Buffer.concat(chunks);
-		console.log(body.toString());
-	});
-});
-
-req.end();
