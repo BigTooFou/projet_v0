c24ad49bd70f09dc8555ebcdfc74f1cd9794aada
Paul Faurie <paul.faurie@yahoo.fr>
2021-02-09 22:56:00 +0100

    test de mort

--- server.js
+++ server.js
@@ -17,41 +17,17 @@ const random_wait_time = (waitTime = 300) => new Promise((resolve, reject) => {
     }, Math.random() * waitTime)
   })
   
-  
-  const get_followers = async(userId, userFollowerCount) => {
-    let userFollowers = [],
-      batchCount = 20,
-      actuallyFetched = 20,
-      url = `https://www.instagram.com/graphql/query/?query_hash=c76146de99bb02f6415203be841dd25a&variables={"id":"${userId}","include_reel":true,"fetch_mutual":true,"first":"${batchCount}"}`
-    while (userFollowerCount > 0) {
-      const followersResponse = await fetch(url)
-        .then(res => res.json())
-        .then(res => {
-          const nodeIds = []
-          for (const node of res.data.user.edge_followed_by.edges) {
-            nodeIds.push(node.node.id)
-          }
-          actuallyFetched = nodeIds.length;
-          return {
-            edges: nodeIds,
-            endCursor: res.data.user.edge_followed_by.page_info.end_cursor
-          }
-        }).catch(err => {
-          userFollowerCount = -1
-          return {
-            edges: []
-          }
-        })
-      await random_wait_time()
-      userFollowers = [...userFollowers, ...followersResponse.edges]
-      userFollowerCount -= actuallyFetched
-      url = `https://www.instagram.com/graphql/query/?query_hash=c76146de99bb02f6415203be841dd25a&variables={"id":"${userId}","include_reel":true,"fetch_mutual":true,"first":${batchCount},"after":"${followersResponse.endCursor}"}`
-    }
-    console.log(userFollowers)
-    return userFollowers
-  }
 
-console.log(get_followers(7267243669, 10))
+  const instaObj = require('instagram-basic-data-scraper-with-username')
+  const user = 'paul_faurie';
+
+  instaObj.getFollowing(user).then(res => {
+    const userFollowing = res.data;
+    console.log(userFollowing);
+    // => Joie
+  });
+
+
 
 app.listen(2502)
 
